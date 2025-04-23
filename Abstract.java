9bc634e66acedb2dcc9c28ec4fce40c875aaa3ad
Vamika2314 <156651563+Vamika2314@users.noreply.github.com>
2025-04-23 20:13:59 +0530

    Add files via upload

---
+++ Abstract.java
@@ -0,0 +1,41 @@
+/* Implementation of abstraction using abstract class
+abstract class Animal {
+     abstract void makeSound();
+     void sleep() {
+         System.out.println("Sleeping...");
+     }
+ }
+
+ class Dog extends Animal {
+     @Override
+     void makeSound() {
+         System.out.println("Bark");
+     }
+ }
+
+ public class Abstract {
+     public static void main(String[] args) {
+         Animal myDog = new Dog();
+         myDog.makeSound();
+         myDog.sleep();
+     }
+ }  */
+
+// Implementation of abstraction using interface
+interface Vehicle {
+    void start();
+}
+
+class Car implements Vehicle {
+    @Override
+    public void start() {
+        System.out.println("Car starting...");
+    }
+}
+
+public class Abstract {
+    public static void main(String[] args) {
+        Vehicle myCar = new Car();
+        myCar.start();
+    }
+}
